149ebcbc524fca31dd8d2b402d78977c34142494
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-07-03 22:34:12 -0300

    desafio constraint layout feito

--- app/src/main/java/com/example/motivation/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/views/MainActivity.kt
@@ -2,29 +2,21 @@ package com.example.motivation.ui.views
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.motivation.R
-import com.example.motivation.data.repository.PhrasesRepository
 import com.example.motivation.databinding.ActivityMainBinding
-import com.example.motivation.helpers.Constantes
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding: ActivityMainBinding;
-    private val phrasesRepository: PhrasesRepository = PhrasesRepository()
-
-    private var filter: Int = Constantes.PHRASE.PHRASE_ALL
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -33,63 +25,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             insets
         }
 
-        var nomeUsuario = binding.texViewName
-        nomeUsuario.text = "Olá ${intent.extras?.getString("usuario") ?: "Usuario nao encontrado"}"
-        setListeners()
-        //deixando marcado no inicio porque nao tem filtro selecionado ainda, ou seja, todos(ALL)
-        filterPhrases(R.id.imageView_inclusive)
-        handleNewSentence()
     }
 
     override fun onClick(view: View) {
 
-        //criando lista com os IDs do xml para validar click
-        val listId = listOf(R.id.imageView_light, R.id.imageView_insert, R.id.imageView_inclusive)
-
-        if (view.id == R.id.button_new_sentence) {
-            handleNewSentence()
-        }else if(view.id in listId){
-            filterPhrases(view.id)
-        }
-    }
-
-    private fun filterPhrases(id: Int) {
-
-        //colocando todos icones preto e depois a logica e executada para marcar novamente
-        binding.imageViewLight.setColorFilter(ContextCompat.getColor(this,R.color.black))
-        binding.imageViewInsert.setColorFilter(ContextCompat.getColor(this,R.color.black))
-        binding.imageViewInclusive.setColorFilter(ContextCompat.getColor(this,R.color.black))
-
-        when(id){
-            R.id.imageView_light -> {
-                filter = Constantes.PHRASE.PHRASE_SUNNY
-                binding.imageViewLight.setColorFilter(ContextCompat.getColor(this,R.color.white))
-            }
-            R.id.imageView_inclusive ->{
-                filter = Constantes.PHRASE.PHRASE_ALL
-                binding.imageViewInclusive.setColorFilter(ContextCompat.getColor(this, R.color.white))
-            }
-            R.id.imageView_insert ->{
-                filter = Constantes.PHRASE.PHRASE_HAPPY
-                binding.imageViewInsert.setColorFilter(ContextCompat.getColor(this, R.color.white))
-            }
-        }
-    }
-
-    private fun handleNewSentence() {
-        binding.textViewSentences.text = phrasesRepository.obterFrase(filter)
-        if(binding.textViewSentences.text.equals(Constantes.MENSAGENS.LISTA_VAZIA)){
-            Toast.makeText(this, "Lista de frases vazia", Toast.LENGTH_LONG).show()
-        }
     }
 
 
-    private fun setListeners() {
-        binding.buttonNewSentence.setOnClickListener(this)
-        binding.imageViewLight.setOnClickListener(this)
-        binding.imageViewInsert.setOnClickListener(this)
-        binding.imageViewInclusive.setOnClickListener(this)
-    }
 
 
 }
